f642fc5679b3132fe8b085bf0ec8475503fcc8de
Sebas320 <cbastian320@gmail.com>
2017-04-13 18:46:58 -0500

    Constructor de Partner

--- Marketing Multinivel/src/models/entities/Partner.java
+++ Marketing Multinivel/src/models/entities/Partner.java
@@ -1,21 +1,33 @@
 package models.entities;
 
+import java.util.Date;
+
 public class Partner {
-	
+
 	private int id;
-	private String name ;
+	private String name;
+	private Date birthday;
+
+	public Partner(int id, String name, Date birthday) {
+		this.id = id;
+		this.name = name;
+		this.birthday = birthday;
+	}
+	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
 }
