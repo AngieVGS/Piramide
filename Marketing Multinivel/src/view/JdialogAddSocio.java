6ac02fa7b1c609ce277d5c1993da8af27e30807e
Angye <angi-danitv@hotmail.com>
2017-04-22 18:31:18 -0500

    Creacion de dialogo para agregar socios e implementacion de Herramienta GridSistemm para manejar la interface.

---
+++ Marketing Multinivel/src/view/JdialogAddSocio.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.JDialog;
+
+public class JdialogAddSocio extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	public JdialogAddSocio() {
+		
+	}
+}
