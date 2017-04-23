0399a3541f1785e9e432e63379f1c99705528134
Sebas320 <cbastian320@gmail.com>
2017-04-22 20:01:03 -0500

    avances en Company

--- Marketing Multinivel/src/models/entities/Company.java
+++ Marketing Multinivel/src/models/entities/Company.java
@@ -39,11 +39,37 @@ public class Company {
 	}
 	
 	/**
-	 * Método que agrega un socio y su referido al árbol
+	 * Método que agrega un socio con un referido al árbol
 	 * @param partner es el referido
 	 * @param partnerFather es el socio padre, es decir, quien refirio a partner
 	 */
 	public void addPartner(Partner partner, Partner partnerFather) {
 		partnersTree.add(partner, partnerFather);
+	}
+	
+	/**
+	 * Método que agrega un producto a la lista
+	 * @param product producto a agregar
+	 */
+	public void addProduct(Product product) {
+		productList.add(product);
+	}
+	
+	public Partner deletePartner(Partner partnerToDelete) {
+		//TO-DO REMOVE 
+//		partnersTree.remove(partnerToDelete);
+		return null;
+	}
+	
+	public Order deleteOrder(Order orderToDelete) {
+		//TO-DO REMOVE 
+//		partnersTree.remove(orderToDelete);
+		return null;
+	}
+	
+	public Product deleteProduct(Product productToDelete) {
+		//TO-DO REMOVE 
+//		partnersTree.remove(productToDelete);
+		return null;
 	}
 }
