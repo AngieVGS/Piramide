c5bf06e5d8f18c11856534898330b4298d299207
yulianaboyaca04 <mary.boyaca@uptc.edu.co>
2017-04-22 19:01:44 -0500

    ENUM

--- Marketing Multinivel/src/view/JdialogAddPartner.java
+++ Marketing Multinivel/src/view/JdialogAddPartner.java
@@ -1,12 +1,21 @@
 package view;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
 
 public class JdialogAddPartner extends JDialog {
 
-	private static final long serialVersionUID = 1L;
-
-	public JdialogAddPartner() {
-		
-	}
 }
