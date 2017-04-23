b28dd99c2a84fc6c4eaa7e8a84c87db8d5600d5c
Angye <angi-danitv@hotmail.com>
2017-04-22 22:31:01 -0500

    Avances interface

--- Marketing Multinivel/src/view/ConstantsUI.java
+++ Marketing Multinivel/src/view/ConstantsUI.java
@@ -1,10 +1,32 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.ImageIcon;
 
 public class ConstantsUI {
 
 	public static final ImageIcon IMG_ICON = new ImageIcon("src/img/marketing.png");
 	public static final String TITLE = "Marketing Multilevel";
-
+	public static final String SENTENCE_ID_ORDER = "Id Order";
+	public static final String SENTENCE_FIRST_NAME = "FirstName";
+	public static final String SENTENCE_REGISTER = "Register";
+	public static final String SENTENCE_LAST_NAME = "Last Name";
+	public static final String SENTENCE_GENRE = "Genre";
+	public static final String SENTENCE_NACIMIENTO = "Nacimiento";
+	public static final String SENTENCE_STATUS = "Status";
+	public static final String SENTENCE_PARENT = "Parent";
+	public static final String SENTENCE_LINEA = "Linea";
+	public static final String SENTENCE_REFERENCE = "Reference";
+	public static final String SENTENCE_TARGET = "Target";
+	public static final String SENTENCE_VALUE = "Value";
+	public static final String SENTENCE_ID_PARTNER = "Id Partner";
+	public static final String SENTENCE_DATE = "Date";
+	public static final String SENTENCE_ID_PRODUCT = "Id producto";
+	public static final String SENTENCE_QUANTITY = "Quantity";
+	public static final String SENTENCE_STATE = "State";
+	public static final Color COLOR_FOUND = Color.decode("#00405d");
+	public static final Color COLOR_TABLE_MANAGER = Color.decode("#062f3c");
+	public static final Font FONT_LETTER_TABLE = new Font("Ahaori", Font.BOLD, 18);
 }
--- Marketing Multinivel/src/view/JFrameMainWindow.java
+++ Marketing Multinivel/src/view/JFrameMainWindow.java
@@ -4,23 +4,61 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class JFrameMainWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private Object[] columnsPartners;
+	private Object[] columnsProducts;
+	private Object[] columnsOrders;
 
 	public JFrameMainWindow() {
 		setTitle(ConstantsUI.TITLE);
 		setSize(700, 700);
 		setLayout(new BorderLayout());
 		setExtendedState(MAXIMIZED_BOTH);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		getContentPane().setBackground(Color.WHITE);
 		setIconImage(ConstantsUI.IMG_ICON.getImage());
 		MenuBar menuBar = new MenuBar();
 		setJMenuBar(menuBar);
+
+		columnsPartners = new Object[] { ConstantsUI.SENTENCE_ID_PARTNER, ConstantsUI.SENTENCE_REGISTER,
+				ConstantsUI.SENTENCE_FIRST_NAME, ConstantsUI.SENTENCE_LAST_NAME, ConstantsUI.SENTENCE_GENRE,
+				ConstantsUI.SENTENCE_NACIMIENTO, ConstantsUI.SENTENCE_STATUS, ConstantsUI.SENTENCE_PARENT,
+				ConstantsUI.SENTENCE_PARENT };
+		columnsProducts = new Object[] { ConstantsUI.SENTENCE_ID_PRODUCT, ConstantsUI.SENTENCE_LINEA,
+				ConstantsUI.SENTENCE_REFERENCE, ConstantsUI.SENTENCE_TARGET, ConstantsUI.SENTENCE_VALUE };
+		columnsOrders = new Object[] { ConstantsUI.SENTENCE_ID_ORDER, ConstantsUI.SENTENCE_DATE,
+				ConstantsUI.SENTENCE_ID_PARTNER, ConstantsUI.SENTENCE_QUANTITY, ConstantsUI.SENTENCE_STATE };
+		
+		JPanel pnCentralStudents = new JPanel();
+		pnCentralStudents.setBackground(ConstantsUI.COLOR_FOUND);
+		GridSystem gridSystem = new GridSystem(pnCentralStudents);
+		
+		DefaultTableModel partnersModel = new DefaultTableModel();
+		partnersModel.setColumnIdentifiers(columnsPartners);
+		JTable tbParner = new JTable(partnersModel);
+		
+		tbParner.getTableHeader().setBackground(ConstantsUI.COLOR_TABLE_MANAGER);
+		tbParner.getTableHeader().setFont(ConstantsUI.FONT_LETTER_TABLE);
+		tbParner.getTableHeader().setForeground(Color.WHITE);
+		tbParner.setRowHeight(30);
+		tbParner.setSelectionBackground(Color.decode("#2b82ad"));
+		tbParner.setSelectionForeground(Color.WHITE);
+		
+		JScrollPane panelStudents = new JScrollPane(tbParner);
+		panelStudents.setBackground(Color.WHITE);
+		pnCentralStudents.add(panelStudents, gridSystem.insertComponent(1, 1, 10, 0.15));
+//		pnTabStudents.add(pnCentralStudents, BorderLayout.CENTER);
+		
 		setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
 		new JFrameMainWindow();
 	}
