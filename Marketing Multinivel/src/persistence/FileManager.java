9e451d2a25d394d51239872d93e41b23f2fb8643
Angye <angi-danitv@hotmail.com>
2017-04-25 15:58:47 -0500

    Persistencia Almacenando en arboles binarios y prueba.

--- Marketing Multinivel/src/persistence/FileManager.java
+++ Marketing Multinivel/src/persistence/FileManager.java
@@ -8,15 +8,15 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Date;
-import java.util.ArrayList;
 
+import exceptions.RegisteredPartner;
+import models.comparator.ComparatorPartner;
+import models.dao.Company;
 import models.entities.Genre;
 import models.entities.Partner;
-import structureData.SimpleList;
 
 public class FileManager {
-	
+
 	public String fileName;
 	public File file;
 	public FileReader fr;
@@ -26,115 +26,74 @@ public class FileManager {
 	public PrintWriter pw;
 	public boolean append = true;
 
-	
 	public FileManager(String st) throws IOException {
 		this.fileName = st;
-//		file = new File(fileName);
-//		//		fw = new FileWriter(file, true);
-//		fr = new FileReader(file);
-////		bw = new BufferedWriter(fw);
-		//br =  new BufferedReader(fr);
-//		pw = new PrintWriter(bw);
-		
 	}
-	
-	public void createFile(String nameFile){
+
+	public void createFile(String nameFile) {
 		this.fileName = nameFile;
 	}
-	
-	
-	public void openToWrite() throws IOException{
-		
+
+	public void openToWrite() throws IOException {
 		file = new File(fileName);
 		fw = new FileWriter(file, append);
 		fr = new FileReader(file);
 		bw = new BufferedWriter(fw);
-		
 		fw = new FileWriter(file);
 	}
-	
-	public void openToRead() throws FileNotFoundException{
-		file = new File( fileName);
+
+	public void openToRead() throws FileNotFoundException {
+		file = new File(fileName);
 		fr = new FileReader(file);
 		br = new BufferedReader(fr);
 	}
 
-	public void writeFile(String chain) throws IOException{
-//				pw.write(chain);
-//		fw = new FileWriter(file, true);
-		fw.write(chain + (char)13 +(char)10 );
+	public void writeFile(String chain) throws IOException {
+		fw.write(chain + (char) 13 + (char) 10);
 		fw.flush();
 	}
-	
-	public SimpleList<Partner> read() throws IOException{
-		SimpleList<Partner> auxList= new SimpleList<>();
+
+	public Company readToTree() throws IOException {
+		Company auxList = new Company(new ComparatorPartner());
 		Partner partnerAux;
-		String line ="";
+		String line = "";
 		openToRead();
 		do {
 			try {
 				line = br.readLine();
-				if (line !=null) {
-					if (line.charAt(0)!= 'I') {
-						
-					partnerAux = new Partner(null, Long.parseLong(line.split(",")[2]), line.split(",")[3],line.split(",")[4], Genre.FEMALE, null, Integer.parseInt(line.split(",")[7]), Integer.parseInt(line.split(",")[8]));
-					System.out.println(partnerAux.toString());
-					partnerAux.setId(Integer.parseInt(line.split(",")[0]));
-					auxList.add(partnerAux);
+				if (line != null) {
+					if (line.charAt(0) != 'I') {
+						partnerAux = new Partner(null, Long.parseLong(line.split(",")[2]), line.split(",")[3],
+								line.split(",")[4], Genre.FEMALE, null, Integer.parseInt(line.split(",")[7]),
+								Integer.parseInt(line.split(",")[8]));
+//						System.out.println(partnerAux.toString());
+						partnerAux.setId(Integer.parseInt(line.split(",")[0]));
+						auxList.registerPartner(partnerAux);
 					}
 				}
-				auxList.remove(0);
 			} catch (IOException e) {
-				throw new IOException("error al leer los datos del archivo "+ super.toString());
+				throw new IOException("error al leer los datos del archivo " + super.toString());
+			} catch (RegisteredPartner e) {
+				e.printStackTrace();
 			}
-		} while(line != null);
+		} while (line != null);
 		br.close();
 		fr.close();
-		return  auxList;
-		
+		return auxList;
 	}
 
-//	public  ArrayList<VideoGame>  readFile() throws IOException{
-//		ArrayList<String> arrayList = new ArrayList<String>();
-//		ArrayList<VideoGame> listVideoGame = new ArrayList<>();
-//		VideoGame gameAux = new VideoGame();
-//		String line ="";
-//		openToRead();
-//		do {
-//			try {
-//				line = br.readLine();
-//				if (line !=null) {
-////					arrayList.add(line);
-////					line.split(",");
-//					gameAux = new VideoGame(Integer.parseInt(line.split(",")[0]), line.split(",")[1], file.getName(), line.split(",")[2], 
-//							line.split(",")[3], null,0, 0, 0, 0, Double.parseDouble(line.split(",")[4]));
-//					listVideoGame.add(gameAux);
-//				}
-//			} catch (IOException e) {
-//				throw new IOException("error al leer los datos del archivo "+ super.toString());
-//			}
-//		} while(line != null);
-//		br.close();
-//		fr.close();
-//		return  listVideoGame;
-//	}
-	
-	
-	
-	
-	public void close() throws IOException{
+	public void close() throws IOException {
 		fw.close();
 	}
 
-	
-	public void notAppend(){
+	public void notAppend() {
 
 	}
-	
+
 	public String getFileName() {
 		return fileName;
 	}
-	
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
@@ -142,7 +101,6 @@ public class FileManager {
 	public boolean isAppend() {
 		return append;
 	}
-
 
 	public void setAppend(boolean append) {
 		this.append = append;
--- Marketing Multinivel/src/test/TestPersistence.java
+++ Marketing Multinivel/src/test/TestPersistence.java
@@ -2,23 +2,18 @@ package test;
 
 import java.io.IOException;
 
+import models.dao.Company;
 import models.entities.Partner;
 import persistence.FileManager;
 import structureData.SimpleList;
 
-public class initialValues {
+public class TestPersistence {
 
 	public static void main(String[] args) {
-		
-		
-		System.out.println(Integer.MAX_VALUE);
 		try {
 			FileManager fileManager = new FileManager(".\\src\\data\\multinivelSocios.csv");
-			SimpleList<Partner> list = fileManager.read();
-			
-			for (int i = 1; i < list.size(); i++) {
-				System.out.println(list.get(i).toString());
-			}
+			Company company = fileManager.readToTree();
+			company.imprimir();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
